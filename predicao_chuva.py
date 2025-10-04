077108404e2b0d8187a5d8ceb6729464feb62f3b
Isabel Cristina <isabelmarras13@gmail.com>
2025-10-04 18:57:27 -0300

    gera csv com a previsao

--- predicao_chuva.py
+++ predicao_chuva.py
@@ -20,7 +20,6 @@ df["ds"] = pd.to_datetime(df["ds"], format="%Y%m%d")
 df = df.sort_values("ds").reset_index(drop=True)
 
 df_mes = df.set_index("ds").resample("ME").sum().reset_index()
-df_mes["y_log"] = np.log(df_mes["y"] + 1)
 
 print(df.head())
 
@@ -31,6 +30,15 @@ model.fit(df_mes)
 
 futuro = model.make_future_dataframe(periods=24, freq="M")
 forecast = model.predict(futuro)
+
+
+forecast[["yhat","yhat_lower","yhat_upper"]] = forecast[["yhat","yhat_lower","yhat_upper"]].clip(lower=0)
+
+forecast[["ds","yhat","yhat_lower","yhat_upper"]].to_csv(
+    "/Users/isabelcristina/Desktop/nasa_space_apps/previsao_chuva_mensal.csv", index=False
+)
+print("CSV criado com sucesso!")
+
 forecast["yhat"] = forecast["yhat"].clip(lower=0)
 forecast["yhat_lower"] = forecast["yhat_lower"].clip(lower=0)
 forecast["yhat_upper"] = forecast["yhat_upper"].clip(lower=0)
@@ -41,13 +49,16 @@ forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
 forecast_futuro = forecast.tail(24)
 print(forecast_futuro[['ds', 'yhat', 'yhat_lower', 'yhat_upper']])
 
+
 fig1 = model.plot(forecast)
 plt.ylim(0,200)
 plt.show()
 fig2 = model.plot_components(forecast)
 plt.show()
 
+print("teste")
 
 forecast[["ds", "yhat", "yhat_lower", "yhat_upper"]].to_csv(
-    "previsao_chuva_mensal.csv", index=False
+    "/Users/isabelcristina/Desktop/nasa_space_apps/previsao_chuva_mensal.csv", index=False
 )
+print("CSV criado com sucesso!")
