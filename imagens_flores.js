908e73037c13d48ef6249132cee805c024de08bd
Isabel Cristina <isabelmarras13@gmail.com>
2025-10-05 17:38:58 -0300

    imagens flores

---
+++ imagens_flores.js
@@ -0,0 +1,9 @@
+const imagensFlores = {
+  "Sucupira-branca": "imagens/Sucupira-branca.png",
+  "Candeia": "imagens/Candeia.png",
+  "Ipê-amarelo": "imagens/Ipê-amarelo.png",
+  "Barbatimão": "imagens/barbatimao.png",
+  "Puçá": "imagens/puca.png",
+  "Tingui": "imagens/Tingui.png",
+  "Velame": "imagens/velame.png"
+};
